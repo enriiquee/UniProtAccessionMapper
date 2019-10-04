d9e6aa1f1071ab2ab1c0ffc3fa8bd8ce129e7110
Enrique Perez <enrique.perez@ucm.es>
2019-10-04 11:58:48 +0200

    Update UniProtAccesionMapper.py

--- UniProtAccesionMapper.py
+++ UniProtAccesionMapper.py
@@ -9,7 +9,7 @@ try:
 	import urllib.request as urllib
 	import pandas as pd
 except ImportError:
-  print("Trying to Install required module: requests\n")
+  #print("Trying to Install required module: requests\n")
   os.system('python -m pip install requests')
 # -- above lines try to install requests module if not present
 # -- if all went well, import required module again ( for global access)
